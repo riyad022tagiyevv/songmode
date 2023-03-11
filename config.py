32aa7787ffa36e6ddd48f659d813cb0defc3ea58
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2023-03-11 21:43:57 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5984042222:AAFOM33lTp7WlKYynROqslNJJV6ZWo6nc5I
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6109967361:AAEXZWE1hSoeHeadEDCiEyZMMuIoF7BDH8w
 ")
-    API_ID = int(os.environ.get("API_ID", "26712413"))
-    API_HASH = os.environ.get("API_HASH", "3298034eb7cec614ef852fda02536153")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "nesirovqadirofficiall")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "QadirSongBot")
+    API_ID = int(os.environ.get("API_ID", "12349641"))
+    API_HASH = os.environ.get("API_HASH", "0f9159afc920f9c592df555e4b1cb73b")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "RiyadAndMe ")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ModernSongBot")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "https://t.me/darksideplay")
     CHANNEL = os.environ.get("CHANNEL", "darksideplay")
     PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001719261837"))
